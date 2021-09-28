8561bb4fd6c4f0b6b066183953751c8ee77f4a02
自分の名前 <>
2021-09-28 23:49:13 +0900

    ページネーションを追加

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -35,12 +35,26 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
         // TODO Auto-generated method stub
-        List<Message>messages = em.createNamedQuery("getAllMessages",Message.class).getResultList();
-        //response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        // 開くページ数を取得（デフォルトは１ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Message> messages = em.createNamedQuery("getAllMessages",Message.class)
+                                .setFirstResult(15 *(page-1))
+                                .setMaxResults(15)
+                                .getResultList();
+        // 全件数を取得
+        long messages_count = (long)em.createNamedQuery("getMessagesCount",Long.class)
+                                .getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count); //全件数
+        request.setAttribute("page", page); // ページ数
 
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除
